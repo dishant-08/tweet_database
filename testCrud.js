50a1ab50c4f8238a07de892a3e03ff66c9046f96
Dishant Sahu <dishant.sahu07@gmail.com>
2023-11-24 00:11:28 +0530

    perform crud

--- testCrud.js
+++ testCrud.js
@@ -1,30 +1,32 @@
 const { User, Post, like } = require("./models");
+//create
+async function createUser() {
+  const user = await User.create({
+    username: "Zoro",
+    display_name: "Pirate Hunter",
+    email: "onepiece.king@gmail.com",
+    bio: "Strongest Swordsman of the world",
+  });
 
-// async function createUser() {
-//   const user = await User.create({
-//     username: "Zoro",
-//     display_name: "Pirate Hunter",
-//     email: "onepiece.king@gmail.com",
-//     bio: "Strongest Swordsman of the world",
-//   });
+  const post = await Post.create({
+    content: "Onigiri , Three thousand ",
+    posted_at: new Date(),
+    user_id: user.id,
 
-//   const post = await Post.create({
-//     content: "Onigiri , Three thousand ",
-//     posted_at: new Date(),
-//     user_id: user.id,
+    type: "post",
+  });
 
-//     type: "post",
-//   });
+  const Like = await like.create({
+    user_id: user.id,
+    post_id: post.id,
 
-//   const Like = await like.create({
-//     user_id: user.id,
-//     post_id: post.id,
-//     liked_at: new Date(),
-//   });
-// }
+    liked_at: new Date(),
+  });
+}
 
-// createUser();
+createUser();
 
+//read
 async function fetchData() {
   const user = await User.findAll();
   console.log(JSON.stringify(user, null, 2));
@@ -32,14 +34,31 @@ async function fetchData() {
 
 fetchData();
 
-// async function updateData() {
-//   await User.update(
-//     { username: "Mihawk", display_name: "Dracula" },
-//     {
-//       where: {
-//         id: 2,
-//       },
-//     }
-//   );
-// }
-// updateData();
+//update
+async function updateData() {
+  await User.update(
+    { username: "Mihawk", display_name: "Dracula" },
+    {
+      where: {
+        id: 2,
+      },
+    }
+  );
+}
+updateData();
+
+//delete
+async function deleteData() {
+  await like.destroy({
+    where: {
+      post_id: 2,
+    },
+  }),
+    await Post.destroy({
+      where: {
+        id: 2,
+      },
+    });
+}
+
+deleteData();
